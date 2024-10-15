fc3144fc906118d82788edcdd0846276418bb007
MyriamKuhn <myriam.kuehn@free.fr>
2024-10-16 01:37:09 +0200

    Add Solaris Pages not filled

--- solaris.php
+++
@@ -1,6 +0,0 @@
-Solaris Prime
-
-    Rôle : Le cœur du système, à la fois source d'énergie et de lumière pour toutes les autres planètes. Elle pourrait être une étoile habitable ou un monde exceptionnel autour duquel tout gravite.
-    Caractéristiques : En tant que planète centrale, Solaris Prime serait le siège de civilisations avancées ou le point de départ des explorations dans le reste du système. Elle pourrait également abriter des mystères ou des défis spécifiques, étant à la fois un symbole de stabilité et de danger potentiel.
-
-Ce nom suggère sa primauté et son importance, tout en évoquant une atmosphère imposante et solaire. Solaris Prime pourrait être le "pivot" autour duquel les mini jeux et les intrigues se développent.
